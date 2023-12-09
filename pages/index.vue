a8fc86e9a7374b2b61f3c185edfe78a4f5abfdb9
sawich <sawich@angeldev.org>
2023-12-09 02:35:52 +0200

    Fix icon size

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,22 @@ import { useItemStore } from "~/stores/items";
 
 const { items } = useItemStore();
 
-const iconOf = (id: string) => ({
-  backgroundImage: `url(./${id}), url(./GUI/Icon/Item/Dummy.png)`,
+const iconOf = (icon: string) => ({
+  backgroundImage: `url(./${icon}), url(./GUI/Icon/Item/Dummy.png)`,
 });
 </script>
 
 <template>
   <section class="flex flex-wrap gap-2" v-if="items !== null">
-    <button
-      class="h-12 w-12 border border-transparent transition-all hover:border-pink-500 hover:opacity-50"
+    <div
+      class="border border-transparent transition-all hover:border-pink-500"
       v-for="{ icon, id } of items.slice(0, 1000)"
-      :style="iconOf(icon)"
       :key="id"
-    />
+    >
+      <button
+        class="h-12 w-12 bg-cover bg-center bg-no-repeat transition-all hover:opacity-50"
+        :style="iconOf(icon)"
+      />
+    </div>
   </section>
 </template>
