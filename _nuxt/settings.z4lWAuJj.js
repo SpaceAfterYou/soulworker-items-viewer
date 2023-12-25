b31289c3b57fcc705c797c45dc6f8897be9eb335
sawich <sawich@users.noreply.github.com>
2023-12-25 20:03:54 +0000

    deploy: 7dd560e3a25ed6cb232fbc0a3b3bdab44d0486ad

---
+++ _nuxt/settings.z4lWAuJj.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.x3n3nnut.js";import{c as t,o as c}from"./entry.2TTGAcnl.js";const n={};function o(s,r){return c(),t("section",null," page settings ")}const f=e(n,[["render",o]]);export{f as default};
