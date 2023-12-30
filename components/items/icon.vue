766d2bcfb7ff09d10dae29f5c441538029267580
sawich <sawich@angeldev.org>
2023-12-30 15:20:53 +0200

    remove dummy icon for transparent bg icons

--- components/items/icon.vue
+++ components/items/icon.vue
@@ -3,16 +3,33 @@ type Props = {
   icon: string;
 };
 
-defineProps<Props>();
+const { icon } = defineProps<Props>();
 
-const iconOf = (icon: string) => ({
-  backgroundImage: `url(./${icon}), url(./GUI/Icon/Item/Dummy.png)`,
-});
+const src = ref<string | null>(null);
+
+const width = ref(0);
+const height = ref(0);
+
+const img = new Image();
+
+img.onload = () => {
+  src.value = `./${icon}`;
+
+  width.value = img.naturalWidth;
+  height.value = img.naturalHeight;
+};
+
+img.src = icon;
 </script>
 
 <template>
   <button class="h-min w-min outline transition-all hover:outline-pink-500" type="button">
-    <div class="h-12 w-12 bg-contain bg-center bg-no-repeat transition-opacity hover:opacity-50" :style="iconOf(icon)">
+    <!-- <div class="h-12 w-12 bg-contain bg-center bg-no-repeat transition-opacity hover:opacity-50" :style="iconOf(icon)"> -->
+    <div class="flex h-14 w-14 place-content-center place-items-center transition-opacity hover:opacity-50">
+      <div class="flex place-content-center place-items-center">
+        <img :height="height" :width="width" :src="src" v-if="src" />
+        <img src="/GUI/Icon/Item/Dummy.png" height="48" width="48" v-else />
+      </div>
       <slot />
     </div>
   </button>
