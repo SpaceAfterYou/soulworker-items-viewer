17434508132393b5893f080cc16e9036886fc1f3
sawich <sawich@users.noreply.github.com>
2024-03-21 14:53:35 +0000

    deploy: 00d3332c5fa0150065d195791ec00700ec748c70

--- _nuxt/BfS1czyg.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c as t,o as c}from"./BNHrgVDO.js";const n={};function o(s,r){return c(),t("section",null," page settings ")}const f=e(n,[["render",o]]);export{f as default};
---
+++ _nuxt/C6yZOvbY.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{c as t,o as c}from"./4ywzT9Yj.js";const n={};function o(s,r){return c(),t("section",null," page settings ")}const f=e(n,[["render",o]]);export{f as default};
